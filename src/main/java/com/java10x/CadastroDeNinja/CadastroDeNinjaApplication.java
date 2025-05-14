c9a5adfd5c34e8962b48831c44ffd0cdb8d1bcdc
tulio <tulio-evangelista@outlook.com>
2025-05-14 02:21:24 -0300

    Primeiro commit

---
+++ src/main/java/com/java10x/CadastroDeNinja/CadastroDeNinjaApplication.java
@@ -0,0 +1,19 @@
+package com.java10x.CadastroDeNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjaApplication.class, args);
+
+		System.out.println("hello world");
+
+
+
+	}
+
+
+}
