af4b1f1b0d97de795c21ad4e26357de772514600
tulio <tulio-evangelista@outlook.com>
2025-06-17 02:01:48 -0300

    feat: Adiciona rotas do controller com o mock

--- src/main/java/com/java10x/CadastroDeNinja/Missoes/MissoesController/MissoesController.java
+++ src/main/java/com/java10x/CadastroDeNinja/Missoes/MissoesController/MissoesController.java
@@ -1,4 +1,34 @@
 package com.java10x.CadastroDeNinja.Missoes.MissoesController;
 
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("missoes/")
 public class MissoesController {
+
+
+    @PostMapping("/criarMissao")
+    public String criarMissao() {
+        return "Missão criada com sucesso!";
+    };
+
+
+
+    @GetMapping("/mostrarTodasMissoes")
+    public String mostrarTodasMissoes() {
+        return "Lista de todas as missões";
+    };
+
+
+    @DeleteMapping("/deletarMissao")
+    public String deletarMissao() {
+        return "Missão deletada com sucesso!";
+    };
+
+
 }
--- src/main/java/com/java10x/CadastroDeNinja/Ninjas/Controller/NinjaController.java
+++ src/main/java/com/java10x/CadastroDeNinja/Ninjas/Controller/NinjaController.java
@@ -1,6 +1,8 @@
 package com.java10x.CadastroDeNinja.Ninjas.Controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,39 @@ public class NinjaController {
     public String primeiraRota(){
         return "Primeira Rota";
     }
+
+    //adicionar ninja(CREATE)
+    @PostMapping("/criarNinja")
+    public String criarNinja() {
+        return "Ninja criado com sucesso!";
+    }
+
+    //procurar ninja por id(CREATE)
+    @GetMapping("/procurarNinjaPorId")
+    public String procurarNinjaPorId() {
+        return "Ninja encontrado com sucesso!";
+    }
+
+    //mostrar todos os ninjas(READ)
+    @GetMapping("/mostrarTodosNinjas")
+    public String mostrarTodosNinjas() {
+        return "Lista de todos os ninjas";
+    }
+
+    //alterar dados do ninja(UPDATE)
+    @PostMapping("/alterarNinja")
+    public String alterarNinja() {
+        return "Ninja alterado com sucesso!";
+    }
+
+    //deletar ninja por id(DELETE)
+    @DeleteMapping("/deletarNinja")
+    public String deletarNinja() {
+        return "Ninja deletado com sucesso!";
+    }
+
+
+
 
 
 }
